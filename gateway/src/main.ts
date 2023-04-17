addd5e14d729d3e2843c87bb3bdc6f5f57224825
theSkyseS <--add>
2023-04-17 22:26:21 +0600

    feat: set up swagger documentation initialization for gateway

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('nestjs-learning')
+    .setDescription('API Documentation')
+    .build();
+
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/docs', app, swaggerDoc);
+
   await app.listen(process.env.GATEWAY_PORT || 5000);
 }
 bootstrap();
