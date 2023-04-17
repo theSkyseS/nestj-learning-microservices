c70d336264c4cccba27c2b34a4e5366ea0319ace
theSkyseS <bugaev.andrei55@gmail.com>
2023-04-17 16:49:46 +0600

    feat: implemented RolesController

--- auth-service/src/roles/roles.controller.ts
+++ auth-service/src/roles/roles.controller.ts
@@ -1,41 +1,24 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from '../auth/auth.guard';
-import { Roles } from '../auth/roles.decorator';
-import { RolesGuard } from '../auth/roles.guard';
+import { Controller } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
-import { RoleModel } from './roles.model';
 import { RolesService } from './roles.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
-@ApiTags('Roles')
-@UseGuards(RolesGuard)
-@UseGuards(AuthGuard)
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
-  @ApiOperation({ summary: 'Creates a new role' })
-  @ApiResponse({ status: 201, type: RoleModel })
-  @ApiBody({ type: CreateRoleDto })
-  @Roles('ADMIN')
-  @Post()
-  create(@Body() dto: CreateRoleDto) {
+  @MessagePattern('roles.create')
+  create(@Payload() dto: CreateRoleDto) {
     return this.rolesService.createRole(dto);
   }
 
-  @ApiOperation({ summary: 'Retrieves all roles from the Database' })
-  @ApiResponse({ status: 200, type: [RoleModel] })
-  @Get()
+  @MessagePattern('roles.getAll')
   getAll() {
     return this.rolesService.getAllRoles();
   }
 
-  @ApiOperation({
-    summary: 'Retrieves role with specified name from the Database',
-  })
-  @ApiResponse({ status: 200, type: [RoleModel] })
-  @Get(':name')
-  getByName(@Param('name') name: string) {
+  @MessagePattern('roles.getByName')
+  getByName(@Payload('name') name: string) {
     return this.rolesService.getRoleByName(name);
   }
 }
