562052f37ee0e6bdf1c0a40bee83a71e54e53749
theSkyseS <bugaev.andrei55@gmail.com>
2023-04-17 16:54:35 +0600

    chores:

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -3,6 +3,7 @@ import { AuthGatewayModule } from './auth-gateway/auth-gateway.module';
 import { UsersGatewayModule } from './users-gateway/users-gateway.module';
 import { ProfilesGatewayModule } from './profiles-gateway/profiles-gateway.module';
 import { AuthModule } from './auth/auth.module';
+import { RolesGatewayModule } from './roles-gateway/roles-gateway.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
     UsersGatewayModule,
     ProfilesGatewayModule,
     AuthModule,
+    RolesGatewayModule,
   ],
 })
 export class AppModule {}
