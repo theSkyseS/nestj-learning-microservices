0af31a59df6879e26420a8d5c43fcb7fd68253fc
theSkyseS <--add>
2023-04-17 22:26:04 +0600

    feat: added swagger documentation for users-gateway

--- gateway/src/users-gateway/users-gateway.controller.ts
+++ gateway/src/users-gateway/users-gateway.controller.ts
@@ -15,6 +15,13 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { Roles } from '../auth/roles.decorator';
 import { AuthGuard } from '../auth/auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
+import {
+  ApiBody,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersGatewayController {
@@ -23,16 +30,41 @@ export class UsersGatewayController {
     @Inject('PROFILE_SERVICE') private readonly profileService: ClientProxy,
   ) {}
 
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returned users',
+  })
   @Get()
   getAll() {
     return this.authService.send('users.getAll', {});
   }
 
+  @ApiOperation({ summary: 'Login user' })
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Returned user',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request',
+  })
   @Get(':id')
   getOne(@Param('id') id: string) {
     return this.authService.send('users.getOne', id);
   }
 
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request',
+  })
   @UseGuards(AuthGuard, RolesGuard)
   @Roles('ADMIN')
   @Post()
@@ -40,6 +72,8 @@ export class UsersGatewayController {
     return this.authService.send('users.create', userDto);
   }
 
+  @ApiOperation({ summary: 'Update a user by ID' })
+  @ApiOkResponse({ description: 'The updated user', type: UpdateUserDto })
   @UseGuards(AuthGuard, RolesGuard)
   @Roles('ADMIN')
   @Put(':id')
@@ -47,6 +81,11 @@ export class UsersGatewayController {
     return this.authService.send('users.update', { id, userDto });
   }
 
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiResponse({
+    status: 200,
+    description: 'The user has been successfully removed.',
+  })
   @UseGuards(AuthGuard, RolesGuard)
   @Roles('ADMIN')
   @Delete(':id')
@@ -54,6 +93,18 @@ export class UsersGatewayController {
     return this.authService.send('users.remove', id);
   }
 
+  @ApiOperation({ summary: 'Deletes user' })
+  @ApiResponse({ status: 200 })
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiBody({
+    description: 'Role name to remove',
+    schema: {
+      type: 'object',
+      properties: {
+        role: { type: 'string' },
+      },
+    },
+  })
   @UseGuards(AuthGuard, RolesGuard)
   @Roles('ADMIN')
   @Post(':id/roles/remove')
@@ -61,6 +112,12 @@ export class UsersGatewayController {
     return this.authService.send('users.removeRole', { id, role });
   }
 
+  @ApiOperation({ summary: 'Adds role to a user' })
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiResponse({
+    status: 200,
+    description: 'The role has been successfully added.',
+  })
   @UseGuards(AuthGuard, RolesGuard)
   @Roles('ADMIN')
   @Post(':id/roles/add')
@@ -68,6 +125,12 @@ export class UsersGatewayController {
     return this.authService.send('users.addRole', { userId: id, role });
   }
 
+  @ApiOperation({ summary: 'Get user profile' })
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returned user profile',
+  })
   @UseGuards(AuthGuard, RolesGuard)
   @Roles('ADMIN')
   @Get(':id/profile')
