e9fd8da50bf41c84eff9b6198b5d6055c5c4ef99
theSkyseS <bugaev.andrei55@gmail.com>
2023-04-21 13:43:00 +0600

    feat: added global transactions to profile-service

--- profile-service/src/main.ts
+++ profile-service/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
+import { Sequelize } from 'sequelize';
+import * as cls from 'cls-hooked';
 
 async function bootstrap() {
+  const namespace = cls.createNamespace('profiles');
+  Sequelize.useCLS(namespace);
+
   const app = await NestFactory.createMicroservice(AppModule, {
     name: 'PROFILE_SERVICE',
     transport: Transport.RMQ,
