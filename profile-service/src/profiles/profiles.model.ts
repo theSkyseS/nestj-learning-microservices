02682fcf81c9f66ce9e5d83d9fdfdd8dbdba2ad7
theSkyseS <bugaev.andrei55@gmail.com>
2023-04-21 13:43:30 +0600

    fix: removed unneeded dependencies in ProfilesModel

--- profile-service/src/profiles/profiles.model.ts
+++ profile-service/src/profiles/profiles.model.ts
@@ -1,10 +1,4 @@
-import {
-  BelongsTo,
-  Column,
-  DataType,
-  Model,
-  Table,
-} from 'sequelize-typescript';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
 interface ProfileModelCreationAttr {
   name: string;
